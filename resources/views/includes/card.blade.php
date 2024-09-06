f60bf43bba8ee2e0ec74e45bb60eb5cd48765a98
aspsptyd <devadiroot@gmail.com>
2024-09-06 20:50:18 +0700

    Update: pemisahan beberapa element dan komponen agar lebih mudah di maintain

---
+++ resources/views/includes/card.blade.php
@@ -0,0 +1,321 @@
+<!-- Card Monitor -->
+<div class="col-sm-6 col-lg-3">
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            <div class="subheader">Report Tasks</div>
+        </div>
+
+        <div class="d-flex align-items-baseline">
+            <div class="h1 mb-3 text-green">55%</div>
+            <div class="me-auto">
+                <span class="text-green d-inline-flex align-items-center lh-1">
+                    &nbsp;task done <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                    &nbsp;<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-check"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>
+                </span>
+            </div>
+        </div>
+
+    <div class="d-flex mb-2">
+        <div>Task All Team Need to Check</div>
+        <div class="ms-auto">
+        <a href="#">
+            <span class="text-red d-inline-flex align-items-center lh-1">
+            {{ 100-55 }}% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up --><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /><path d="M13 18l6 -6" /><path d="M13 6l6 6" /></svg>
+            </span>
+        </a>
+        </div>
+    </div>
+    <div class="progress progress-sm">
+        <div class="progress-bar bg-primary" style="width: 55%" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100" aria-label="55% Complete">
+        <span class="visually-hidden">55% Complete</span>
+        </div>
+    </div>
+    </div>
+</div>
+</div>
+
+<div class="col-sm-6 col-lg-3">
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            <div class="subheader">Akumulasi Report ITSA</div>
+            <div class="ms-auto lh-1">
+        </div>
+        </div>
+        <div class="d-flex align-items-baseline">
+            <div class="h1 mb-0 me-2 text-red">58</div>
+            <div class="me-auto">
+            <span class="text-red d-inline-flex align-items-center lh-1">
+                issue or crash <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                &nbsp;<svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trending-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
+            </span>
+            </div>
+
+            <div class="h1 mb-0 me-2 text-green">35</div>
+
+            <div class="me-auto">
+            <span class="text-green d-inline-flex align-items-center lh-1">
+                fixed <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
+            </span>
+            </div>
+
+        </div>
+    </div>
+    <div id="chart-report-itsa" class="chart-sm"></div>
+</div>
+</div>
+
+<div class="col-sm-6 col-lg-3">
+<div class="card">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            <div class="subheader">Documentation</div>
+        </div>
+        <div class="d-flex align-items-baseline">
+            <div class="h1 mb-3 me-2">10</div>
+            <div class="me-auto">
+                <span class="text-green d-inline-flex align-items-center lh-1">
+                    5% <!-- Download SVG icon from http://tabler-icons.io/i/minus -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
+                </span>
+            </div>
+        </div>
+    </div>
+    <div id="chart-revenue-bg" class="chart-sm"></div>
+</div>
+</div>
+
+<div class="col-sm-6 col-lg-3">
+<div class="card">
+    <div class="card-body">
+    <div class="d-flex align-items-center">
+        <div class="subheader">Release Products</div>
+    </div>
+    <div class="d-flex align-items-baseline">
+        <div class="h1 mb-3 me-2">125</div>
+        <div class="me-auto">
+        <span class="text-green d-inline-flex align-items-center lh-1">
+            4% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon ms-1" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 17l6 -6l4 4l8 -8" /><path d="M14 7l7 0l0 7" /></svg>
+            &nbsp; released
+        </span>
+        ~
+        <span class="text-green d-inline-flex align-items-center lh-1">
+            2% <!-- Download SVG icon from http://tabler-icons.io/i/trending-up -->
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trending-down"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7l6 6l4 -4l8 8" /><path d="M21 10l0 7l-7 0" /></svg>
+            &nbsp; rejected
+        </span>
+        </div>
+    </div>
+    </div>
+    <div id="chart-active-users" class="chart-sm"></div>
+</div>
+</div>
+
+<!-- Card Pencapaian -->
+@if (\Auth::user()->roles === 'member')
+    
+@else
+<div class="col-12">
+    <div class="row row-cards">
+        <div class="col-sm-6 col-lg-3">
+        <div class="card card-sm">
+            <div class="card-body">
+            <div class="row align-items-center">
+                <div class="col-auto">
+                <span class="bg-cyan text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-status-change"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M18 18m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M6 12v-2a6 6 0 1 1 12 0v2" /><path d="M15 9l3 3l3 -3" /></svg>
+                </span>
+                </div>
+                <div class="col">
+                <div class="font-weight-medium">
+                    132 incoming change request
+                </div>
+                <div class="text-secondary">
+                    13 closed
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+        <div class="card card-sm">
+            <div class="card-body">
+            <div class="row align-items-center">
+                <div class="col-auto">
+                <span class="bg-blue text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/shopping-cart -->
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-users"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" /></svg>
+                </span>
+                </div>
+                <div class="col">
+                <div class="font-weight-medium">
+                    5 members
+                </div>
+                <div class="text-secondary">
+                    4 active
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+        <div class="card card-sm">
+            <div class="card-body">
+            <div class="row align-items-center">
+                <div class="col-auto">
+                <span class="bg-orange text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-twitter -->
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-firebase"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4.53 17.05l6.15 -11.72h-.02c.38 -.74 1.28 -1.02 2.01 -.63c.26 .14 .48 .36 .62 .62l1.06 2.01" /><path d="M15.47 6.45c.58 -.59 1.53 -.59 2.11 -.01c.22 .22 .36 .5 .41 .81l1.5 9.11c.1 .62 -.2 1.24 -.76 1.54l-6.07 2.9c-.46 .25 -1.01 .26 -1.46 0l-6.02 -2.92c-.55 -.31 -.85 -.92 -.75 -1.54l1.96 -12.04c.12 -.82 .89 -1.38 1.7 -1.25c.46 .07 .87 .36 1.09 .77l1.24 1.76" /><path d="M4.57 17.18l10.93 -10.68" /></svg>
+                </span>
+                </div>
+                <div class="col">
+                <div class="font-weight-medium">
+                    320 open report firebase
+                </div>
+                <div class="text-secondary">
+                    120 fixed close
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="col-sm-6 col-lg-3">
+        <div class="card card-sm">
+            <div class="card-body">
+            <div class="row align-items-center">
+                <div class="col-auto">
+                <span class="bg-dark text-white avatar"><!-- Download SVG icon from http://tabler-icons.io/i/brand-facebook -->
+                    <svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-notebook"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18" /><path d="M13 8l2 0" /><path d="M13 12l2 0" /></svg>
+                </span>
+                </div>
+                <div class="col">
+                <div class="font-weight-medium">
+                    54 notes & mom
+                </div>
+                <div class="text-secondary">
+                    13 closed
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endif
+
+
+<!-- Social Media Traffic & Tasks -->
+<div class="col-lg-5">
+    <div class="card">
+        <div class="card-body">
+        <h3 class="card-title">Traffic report</h3>
+        <div id="chart-mentions" class="chart-lg"></div>
+        </div>
+    </div>
+</div>
+<div class="col-7">
+    <div class="card" style="height: 21rem">
+        <div class="card-header">
+            <h3 class="card-title">Task Update Team</h3>
+        </div>
+        <div class="card-body card-body-scrollable card-body-scrollable-shadow">
+            <div class="divide-y">
+                <div>
+                    <div class="row">
+                        <div class="col-auto">
+                        <span class="avatar">JL</span>
+                        </div>
+                        <div class="col">
+                        <div class="text-truncate">
+                            <strong>Jeffie Lewzey</strong> commented on your <strong>"I'm not a witch."</strong> post.
+                        </div>
+                        <div class="text-secondary">yesterday</div>
+                        </div>
+                        <div class="col-auto align-self-center">
+                        <div class="badge bg-primary"></div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                <div class="row">
+                    <div class="col-auto">
+                    <span class="avatar" style="background-image: url({{ asset('tabler-admin') }}/static/avatars/002m.jpg)"></span>
+                    </div>
+                    <div class="col">
+                    <div class="text-truncate">
+                        It's <strong>Mallory Hulme</strong>'s birthday. Wish him well!
+                    </div>
+                    <div class="text-secondary">2 days ago</div>
+                    </div>
+                    <div class="col-auto align-self-center">
+                    <div class="badge bg-primary"></div>
+                    </div>
+                </div>
+                </div>
+                <div>
+                <div class="row">
+                    <div class="col-auto">
+                    <span class="avatar" style="background-image: url({{ asset('tabler-admin') }}/static/avatars/003m.jpg)"></span>
+                    </div>
+                    <div class="col">
+                    <div class="text-truncate">
+                        <strong>Dunn Slane</strong> posted <strong>"Well, what do you want?"</strong>.
+                    </div>
+                    <div class="text-secondary">today</div>
+                    </div>
+                    <div class="col-auto align-self-center">
+                    <div class="badge bg-primary"></div>
+                    </div>
+                </div>
+                </div>
+                <div>
+                <div class="row">
+                    <div class="col-auto">
+                    <span class="avatar" style="background-image: url({{ asset('tabler-admin') }}/static/avatars/000f.jpg)"></span>
+                    </div>
+                    <div class="col">
+                    <div class="text-truncate">
+                        <strong>Emmy Levet</strong> created a new project <strong>Morning alarm clock</strong>.
+                    </div>
+                    <div class="text-secondary">4 days ago</div>
+                    </div>
+                    <div class="col-auto align-self-center">
+                    <div class="badge bg-primary"></div>
+                    </div>
+                </div>
+                </div>
+                <div>
+                <div class="row">
+                    <div class="col-auto">
+                    <span class="avatar" style="background-image: url({{ asset('tabler-admin') }}/static/avatars/001f.jpg)"></span>
+                    </div>
+                    <div class="col">
+                    <div class="text-truncate">
+                        <strong>Maryjo Lebarree</strong> liked your photo.
+                    </div>
+                    <div class="text-secondary">2 days ago</div>
+                    </div>
+                </div>
+                </div>
+                <div>
+                <div class="row">
+                    <div class="col-auto">
+                    <span class="avatar">EP</span>
+                    </div>
+                    <div class="col">
+                    <div class="text-truncate">
+                        <strong>Egan Poetz</strong> registered new client as <strong>Trilia</strong>.
+                    </div>
+                    <div class="text-secondary">yesterday</div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
